097b5e20cf74edcf80b376d39a6798a26e86d13f
thecheneral <jonjchen@gmail.com>
2016-01-07 21:29:08 -0500

    debugging controller

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -2,21 +2,20 @@ class OmniauthController < ApplicationController
   def create
     
     puts request.env['omniauth2.auth'].inspect
-    puts auth_hash.inspect
 
-    auth_hash = request.env['omniauth2.auth']
+    # auth_hash = request.env['omniauth2.auth']
     
-    #session[:user_id]
-    # Means our user is signed in. Add the authorization to the user
-    #User.find(session[:user_id]).add_provider(auth_hash)
-    #auth = User.find_or_create(auth_hash)
+    # #session[:user_id]
+    # # Means our user is signed in. Add the authorization to the user
+    # #User.find(session[:user_id]).add_provider(auth_hash)
+    # #auth = User.find_or_create(auth_hash)
 
-    if current_user
-      current_user.update_attributes access_token: auth_hash["credentials"].secret,
-                                     refresh_token: auth_hash["credentials"].token
-      flash[:notice] = "Fitbit connected!"
-    end
+    # if current_user
+    #   current_user.update_attributes access_token: auth_hash["credentials"].secret,
+    #                                  refresh_token: auth_hash["credentials"].token
+    #   flash[:notice] = "Fitbit connected!"
+    # end
 
-    redirect_to 
+    redirect_to root
   end
 end
