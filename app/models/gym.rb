53d4227d4deef0fbe99939951eb00ce21d917257
thecheneral <jonjchen@gmail.com>
2016-01-09 20:44:38 -0500

    attempting validation on the gym and membership models

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -3,8 +3,7 @@ class Gym < ActiveRecord::Base
 	has_many :memberships
 	has_many :users, :through => :memberships
 
-# validates :default, uniqueness:{ scope: :user,
-# 			message: "Only one default gym per user."}
+
 
 	def name_and_url
 		"#{name} (#{workout_url})"
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,4 +2,6 @@ class Membership < ActiveRecord::Base
 	belongs_to :gym
 	belongs_to :user
 	
+  validates :default, uniqueness:{ scope: :user_id,
+      message: "Only one default gym per user."}
 end
